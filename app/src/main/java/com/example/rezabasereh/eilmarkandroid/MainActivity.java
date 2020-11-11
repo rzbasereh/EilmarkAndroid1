55fecd619be13450a14be380db36d46370e1a5cd
RezaBasereh <rzbasereh1@gmail.com>
2020-11-11 10:15:05 +0330

    Initial commit

---
+++ app/src/main/java/com/example/rezabasereh/eilmarkandroid/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.rezabasereh.eilmarkandroid;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //loading the default fragment
+        loadFragment(new HomeFragment());
+
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    break;
+
+                case R.id.navigation_explore:
+                    fragment = new ExploreFragment();
+                    break;
+
+                case R.id.navigation_cart:
+                    fragment = new CartFragment();
+                    break;
+
+                case R.id.navigation_wish:
+                    fragment = new WishFragment();
+                    break;
+
+                case R.id.navigation_profile:
+                    fragment = new ProfileFragment();
+                    break;
+            }
+
+            return loadFragment(fragment);
+        }
+    };
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+
+}
